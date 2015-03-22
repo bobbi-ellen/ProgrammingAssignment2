bba1f23826e71f47401c8d8fce92bada133edd02
bobbi-ellen <bobbi_ellen@yahoo.com>
2015-03-22 12:17:26 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,51 +1,49 @@
 ## These two functions create a matrix and calculate its inverse.  
 ## Input matrix must be invertible.
-## Given an invertible matrix (b), the inverse is returned by:
-##        cacheSolve(makeCacheMatrix(b))
+
+## Input argument for makeCacheMatrix must be a square matrix; assign result to a variable
+## Resulting variable is input argument for cacheSolve
 
 
 ## This function creates a list containing functions that:
 ##      1.  set - sets the cached matrix to the current matrix
 ##      2.  get - returns the cached matrix (or empty matrix if nothing cached yet)
-##      3.  gatarg - returns the current (input) matrix
-##      4.  setinv - sets the cached inverse to the calculated inverse
-##      5.  getinv - returns the cached inverse
+##      3.  setinv - sets the cached inverse to the calculated inverse
+##      4.  getinv - returns the cached inverse
 
 makeCacheMatrix <- function(mat = matrix()) {
-  set <- function(y) mat_cache <<- y
-  get <- function() {
-    if(exists("mat_cache")) mat_cache     ## Return cached matrix if it exists
-    else {                                ## First time run, mat_cache won't exist yet
-      mat_cache <- matrix(0,0,0)          ## Initialize mat_cache for comparison 
-      mat_cache
-    }
-  } 
-  getarg <- function() mat
-  setinv <- function(inverse) inv_cache <<- inverse
-  getinv <- function() inv_cache
-  list(set = set, get = get,
-    getarg = getarg,
-    setinv = setinv,
-    getinv = getinv)
+ 
+     inv_mat <- NULL
+  
+     set <- function(y) {
+          mat <<- y
+          inv_mat <<- NULL
+     }
+  
+     get <- function() mat
+  
+     setinv <- function(inverse) inv_mat <<- inverse
+  
+     getinv <- function() inv_mat
+  
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## This function returns the inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
   
-  cur_mat <- x$getarg()                   ## Get the current matrix
-  last_mat <- x$get()                     ## Get cached matrix 
+     inv_mat <- x$getinv()
   
-  if(!identical(cur_mat, last_mat)) {     ## Compare cached and current matrices
-    ## Current matrix is not the same as cached matrix; recalculate the inverse
-    x$set(cur_mat)                        ## Set the cached matrix to the current for comparison next time
-    inv_calc <- solve(cur_mat)            ## Calculate the inverse 
-    x$setinv(inv_calc)                    ## Cache the calculated inverse
-    return (inv_calc)                     ## Return the calculated inverse and exit
-  }      
+     if(!is.null(inv_mat)) {                 ## Return cached matrix inverse if it exists
+          message("getting cached data")
+          return(inv_mat)
+     }
   
-  ## Input matrix is the same as previous input, return cached value
-  message("Input matrix same as previous - getting cached inverse.")
-  x$getinv()                              ## Return the cached inverse                  
+     data <- x$get()                         ## Calculate and return matrix inverse if it does not already exist
+     inv_mat <- solve(data, ...)
+      x$setinv(inv_mat)
+     inv_mat
 }
